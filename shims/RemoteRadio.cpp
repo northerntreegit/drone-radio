7de9e3d1c21d9c96398361aa13e16444b8809d4f
northerntreegit <55422582+northerntreegit@users.noreply.github.com>
2025-08-10 17:18:05 +0800

    Create RemoteRadio.cpp

---
+++ shims/RemoteRadio.cpp
@@ -0,0 +1,15 @@
+#include "pxt.h"
+
+extern "C" {
+
+void RemoteRadio_nativeInit() {
+    NRF_RADIO->CRCCNF = 0;
+    NRF_RADIO->PCNF1 &= ~(1 << 25);
+}
+
+void RemoteRadio_nativeShutdown() {
+    NRF_RADIO->CRCCNF = 1;
+    NRF_RADIO->PCNF1 |= (1 << 25);
+}
+
+}
