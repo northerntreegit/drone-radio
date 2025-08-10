70cc3e74d9664d73f7e9d9c4fe6dbd849c48333b
northerntreegit <55422582+northerntreegit@users.noreply.github.com>
2025-08-10 17:10:47 +0800

    Update driver.ts

--- driver.ts
+++ driver.ts
@@ -1,11 +1,11 @@
-namespace radioCore {
-    //% shim=radioCore_nativeInit
+namespace RemoteRadio {
+    //% shim=RemoteRadio_nativeInit
     export function init(): void {
         // 初始化你的 radio 驱动
         // 关闭 CRC、关闭 whitening 等，以便跟 nRF24L01、si24r1 等模块通信
     }
 
-    //% shim=radioCore_nativeShutdown
+    //% shim=RemoteRadio_nativeShutdown
     export function shutdown(): void {
         // 打开 CRC、whitening 功能，恢复原有 radio 设置
     }
