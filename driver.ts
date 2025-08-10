8bbf2f913d6fbfa9ce691a227829df49047db23e
northerntreegit <55422582+northerntreegit@users.noreply.github.com>
2025-08-10 16:07:40 +0800

    Update driver.ts

--- driver.ts
+++ driver.ts
@@ -1,10 +1,10 @@
 namespace radioCore {
     export function init(): void {
         // 初始化你的 radio 驱动
-        // 设置频道、关闭 CRC、关闭 whitening 等
+        // 关闭 CRC、关闭 whitening 等，方便与nrf24L01进行交互
     }
 
     export function shutdown(): void {
-        // 停止接收、关闭 SPI、释放资源等
+        // 打开CRC、打开whitening功能，恢复原radio功能
     }
 }
