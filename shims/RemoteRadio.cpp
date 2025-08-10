94fb2c4be10e556e893b29b34c9232c33e582b69
northerntreegit <55422582+northerntreegit@users.noreply.github.com>
2025-08-10 17:35:18 +0800

    Update RemoteRadio.cpp

--- shims/RemoteRadio.cpp
+++ shims/RemoteRadio.cpp
@@ -13,7 +13,16 @@ void RemoteRadio_nativeShutdown() {
 }
 
 void RemoteRadio_nativeTest() {
-    ;
+    // 设置 P0 为输出模式
+        NRF_GPIO->DIRSET = (1 << 0);
+
+        // 输出 50Hz 方波，持续 5 秒（约 250 个周期）
+        for (int i = 0; i < 250; ++i) {
+            NRF_GPIO->OUTSET = (1 << 0); // 高电平
+            wait_us(10000);              // 等待 10ms
+            NRF_GPIO->OUTCLR = (1 << 0); // 低电平
+            wait_us(10000);              // 等待 10ms
+        }
 }
 
 }
