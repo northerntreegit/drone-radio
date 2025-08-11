16c45951b7679e770051415746289f7b152e301f
northerntreegit <yuting.hit@gmail.com>
2025-08-11 09:58:12 +0800

    简化扩展配置：移除 C++ shims，先确保基本功能能工作

--- driver.ts
+++ driver.ts
@@ -1,16 +1,13 @@
 namespace RemoteRadio {
-    //% shim=RemoteRadio_nativeInit
     export function init(): void {
         // 初始化你的 radio 驱动
         // 关闭 CRC、关闭 whitening 等，以便跟 nRF24L01、si24r1 等模块通信
     }
 
-    //% shim=RemoteRadio_nativeShutdown
     export function shutdown(): void {
         // 打开 CRC、whitening 功能，恢复原有 radio 设置
     }
 
-    //% shim=RemoteRadio_nativeTest
     export function test(): void {
         // 测试
     }
