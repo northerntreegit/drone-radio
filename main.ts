6a02e665b84543a0b6833137e8d769de57c313f8
northerntreegit <yuting.hit@gmail.com>
2025-08-11 09:48:14 +0800

    修复空的 main.ts 文件，恢复扩展功能块定义

---
+++ main.ts
@@ -0,0 +1,23 @@
+//% weight=100 color=#0fbc11 icon="\uf1eb"
+namespace LN_Drone {
+    /** 打开 RemoteRadio */
+    //% block="启用远程无线电"
+    //% group="无线电控制"
+    export function enable_remote(): void {
+        RemoteRadio.init()
+    }
+
+    /** 关闭 RemoteRadio */
+    //% block="禁用远程无线电"
+    //% group="无线电控制"
+    export function disable_remote(): void {
+        RemoteRadio.shutdown()
+    }
+
+    /** 测试 RemoteRadio */
+    //% block="测试远程无线电"
+    //% group="无线电控制"
+    export function test_remote(): void {
+        RemoteRadio.test()
+    }
+}
